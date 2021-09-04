1c2ec721b47d1b446ccb5de292aaad51f454f318
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-04 14:25:22 +0700

    feat: setup lint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import './button.css';
 
 interface ButtonProps {
@@ -33,12 +33,16 @@ export const Button = ({
   backgroundColor,
   label,
   ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+}: ButtonProps): ReactElement => {
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
       style={{ backgroundColor }}
       {...props}
     >
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,1 @@
 export * from './components/Button';
-export * from './stories/Header';
-export * from './stories/Page';
