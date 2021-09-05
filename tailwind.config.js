c49743af84b1b6bf5746d2d2e84f16639fd3ab98
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-05 18:26:49 +0700

    feat: custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   purge: ['./components/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      red: {
+        500: '#d9534f',
+      },
+      cyan: {
+        500: '#53b6e0',
+      },
+      green: {
+        500: '#74c080',
+      },
+      orange: {
+        500: '#f0b518',
+      },
+    },
     extend: {},
   },
   variants: {
