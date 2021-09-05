b0e3c84810669d41b1f9b51b44f6e839935ce939
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-05 23:37:40 +0700

    feat: update button styling

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from './Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -13,20 +12,119 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
+export const Default = Template.bind({});
+Default.args = {
   label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const DefaultDisabled = Template.bind({});
+DefaultDisabled.args = {
   label: 'Button',
+  disabled: true,
+};
+
+export const DefaultOutline = Template.bind({});
+DefaultOutline.args = {
+  label: 'Button',
+  outline: true,
+};
+
+export const DefaultOutlineDisabled = Template.bind({});
+DefaultOutlineDisabled.args = {
+  label: 'Button',
+  outline: true,
+  disabled: true,
+};
+
+export const Info = Template.bind({});
+Info.args = {
+  type: 'info',
+  label: 'Button',
+};
+
+export const InfoDisabled = Template.bind({});
+InfoDisabled.args = {
+  type: 'info',
+  label: 'Button',
+  disabled: true,
+};
+
+export const InfoOutline = Template.bind({});
+InfoOutline.args = {
+  type: 'info',
+  label: 'Button',
+  outline: true,
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  type: 'success',
+  label: 'Button',
+};
+
+export const SuccessDisabled = Template.bind({});
+SuccessDisabled.args = {
+  type: 'success',
+  label: 'Button',
+  disabled: true,
+};
+
+export const SuccessOutline = Template.bind({});
+SuccessOutline.args = {
+  type: 'success',
+  label: 'Button',
+  outline: true,
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  type: 'warning',
+  label: 'Button',
+};
+
+export const WarningDisabled = Template.bind({});
+WarningDisabled.args = {
+  type: 'warning',
+  label: 'Button',
+  disabled: true,
+};
+
+export const WarningOutline = Template.bind({});
+WarningOutline.args = {
+  type: 'warning',
+  label: 'Button',
+  outline: true,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  type: 'error',
+  label: 'Button',
+};
+
+export const ErrorDisabled = Template.bind({});
+ErrorDisabled.args = {
+  type: 'error',
+  label: 'Button',
+  disabled: true,
+};
+
+export const ErrorOutline = Template.bind({});
+ErrorOutline.args = {
+  type: 'error',
+  label: 'Button',
+  outline: true,
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
+  label: 'Button',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
   label: 'Button',
 };
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,48 +1,36 @@
 import React, { ReactElement } from 'react';
-import './button.css';
+import './button.scss';
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
+interface IButtonProps {
+  type?: 'info' | 'success' | 'error' | 'warning' | 'default';
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
+  outline?: boolean;
+  disabled?: boolean;
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
-  primary = false,
+  type = 'default',
   size = 'medium',
+  outline = false,
   backgroundColor,
   label,
   ...props
-}: ButtonProps): ReactElement => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary';
+}: IButtonProps): ReactElement => {
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' '
-      )}
+      className={[
+        'grace-button',
+        `grace-button--${size}`,
+        `${
+          outline
+            ? `grace-button-outline grace-button-outline--${type}`
+            : `grace-button--${type}`
+        }`,
+      ].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
