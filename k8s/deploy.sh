d6452a51f301af57c4a846631dae0d249979800f
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-14 11:27:10 +0700

    feat: update CI

--- k8s/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcloud container clusters get-credentials parley-hub-cluster --region asia-east2-c --project parley-hub-project
-
-envsubst < config.yaml > k8s-config.yaml
-
-kubectl apply -f k8s-config.yaml
